96e8fa293139c3f4e3b898f2d64e1bc0d969377e
2023-1612 <2023-1612@LAB3-PC03>
2026-05-22 12:16:16 +0800

    sgsgsg

--- BantayPilaApp/GuardDashBoardUC.Designer.cs
+++ BantayPilaApp/GuardDashBoardUC.Designer.cs
@@ -28,18 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.cmbPriority = new System.Windows.Forms.ComboBox();
+            this.btnAddPatient = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(52, 94);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(255, 20);
+            this.txtFullName.TabIndex = 0;
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cmbGender.Location = new System.Drawing.Point(71, 141);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(208, 21);
+            this.cmbGender.TabIndex = 1;
+            // 
+            // cmbPriority
+            // 
+            this.cmbPriority.FormattingEnabled = true;
+            this.cmbPriority.Items.AddRange(new object[] {
+            "Standard",
+            "Urgent"});
+            this.cmbPriority.Location = new System.Drawing.Point(71, 196);
+            this.cmbPriority.Name = "cmbPriority";
+            this.cmbPriority.Size = new System.Drawing.Size(208, 21);
+            this.cmbPriority.TabIndex = 2;
+            // 
+            // btnAddPatient
+            // 
+            this.btnAddPatient.Location = new System.Drawing.Point(236, 242);
+            this.btnAddPatient.Name = "btnAddPatient";
+            this.btnAddPatient.Size = new System.Drawing.Size(103, 23);
+            this.btnAddPatient.TabIndex = 3;
+            this.btnAddPatient.Text = "Add to Queue";
+            this.btnAddPatient.UseVisualStyleBackColor = true;
+            this.btnAddPatient.Click += new System.EventHandler(this.btnAddPatient_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "GuardDashBoard";
             // 
             // GuardDashBoardUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddPatient);
+            this.Controls.Add(this.cmbPriority);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.txtFullName);
             this.Name = "GuardDashBoardUC";
-            this.Size = new System.Drawing.Size(369, 551);
+            this.Size = new System.Drawing.Size(369, 323);
+            this.Load += new System.EventHandler(this.GuardDashBoardUC_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.ComboBox cmbPriority;
+        private System.Windows.Forms.Button btnAddPatient;
+        private System.Windows.Forms.Label label1;
     }
 }
--- BantayPilaApp/GuardDashBoardUC.cs
+++ BantayPilaApp/GuardDashBoardUC.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace BantayPilaApp
 {
@@ -15,6 +16,68 @@ namespace BantayPilaApp
         public GuardDashBoardUC()
         {
             InitializeComponent();
+        }
+
+        private void btnAddPatient_Click(object sender, EventArgs e)
+        {
+            // Check if the guard left the name blank
+            if (string.IsNullOrWhiteSpace(txtFullName.Text) || cmbGender.SelectedItem == null || cmbPriority.SelectedItem == null)
+            {
+                MessageBox.Show("Please fill out Name, Gender, and Priority!", "Missing Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Stop the code right here
+            }
+
+            string connString = "Data Source=bantaypila.db;Version=3;";
+
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // 1. Save the Patient to the database
+                    string insertPatientQuery = "INSERT INTO Patients (FullName, Gender) VALUES (@name, @gender)";
+                    using (SQLiteCommand cmdPatient = new SQLiteCommand(insertPatientQuery, conn))
+                    {
+                        cmdPatient.Parameters.AddWithValue("@name", txtFullName.Text);
+                        cmdPatient.Parameters.AddWithValue("@gender", cmbGender.SelectedItem.ToString());
+                        cmdPatient.ExecuteNonQuery();
+                    }
+
+                    // 2. Get the ID of the patient we just created
+                    string getIDQuery = "SELECT last_insert_rowid()";
+                    long newPatientID;
+                    using (SQLiteCommand cmdGetID = new SQLiteCommand(getIDQuery, conn))
+                    {
+                        newPatientID = (long)cmdGetID.ExecuteScalar();
+                    }
+
+                    // 3. Put that patient into the Queue (Visits table)
+                    string insertVisitQuery = "INSERT INTO Visits (PatientID, Priority, Status) VALUES (@patID, @priority, 'Pending_Nurse')";
+                    using (SQLiteCommand cmdVisit = new SQLiteCommand(insertVisitQuery, conn))
+                    {
+                        cmdVisit.Parameters.AddWithValue("@patID", newPatientID);
+                        cmdVisit.Parameters.AddWithValue("@priority", cmbPriority.SelectedItem.ToString());
+                        cmdVisit.ExecuteNonQuery();
+                    }
+
+                    // 4. Success Message & Clear the form for the next person
+                    MessageBox.Show(txtFullName.Text + " has been successfully added to the queue!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txtFullName.Clear();
+                    cmbGender.SelectedIndex = -1;
+                    cmbPriority.SelectedIndex = -1;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Database Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void GuardDashBoardUC_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
